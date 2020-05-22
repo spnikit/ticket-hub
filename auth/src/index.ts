53e637dacf3820d9d9f7e7ecfffd18abeaf4883e
Serge <spnikit@gmail.com>
2020-05-22 20:22:49 +0300

    ingress setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,12 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+// ######### ROUTES ##############
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
+// ######### SERVER FIRE UP ##############
 app.listen(3000, () => {
   console.log("Server is listening on PORT 3000!");
 });
