e5f5661a75690a76e1d3b6d0364241aa1e98b17c
Serge <spnikit@gmail.com>
2020-05-31 19:03:28 +0300

    add notfound error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -15,6 +16,12 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
+// Catch requests to the unspecified routes
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+// Errors go here
 app.use(errorHandler);
 
 // ######### SERVER FIRE UP ##############
