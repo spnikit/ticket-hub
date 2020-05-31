d1279fe41afafcaad28c0b807494225cc7a2b2d6
Serge <spnikit@gmail.com>
2020-05-31 14:13:02 +0300

    auth: add custom errors

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,8 @@
+export class DatabaseConnectionError extends Error {
+  reason = "Error connecting to Database";
+
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
@@ -16,7 +17,7 @@ router.post(
     // REQUEST VALIDATION
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error("Bad credentials, try again!");
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
